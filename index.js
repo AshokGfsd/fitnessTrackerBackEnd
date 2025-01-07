358d8307a7b4a382c5daeae5ca5bd188789decbe
AshokGfsd <166788378+AshokGfsd@users.noreply.github.com>
2025-01-07 14:56:15 +0530

    Update port

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ console.log("connecting to mongo db....");
 
 mongoose.connect(MONGODB_URI).then(() => {
   console.log("connected successfully!");
-  app.listen(4446, () => {
+  app.listen( PORT, () => {
     console.log(`The server is running on http://127.0.0.1:${PORT}`);
   });
 });
